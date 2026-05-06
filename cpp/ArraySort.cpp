a3e8e35a2170bf0b74e62e23d3bb4627b9076483
Muh. Aidil Al Qadri Az <aidilazis25@gmail.com>
2026-05-06 11:17:12 +0700

    Definisi struct Barang, array global, dan fungsi initData

--- cpp/ArraySort.cpp
+++ cpp/ArraySort.cpp
@@ -1,4 +1,47 @@
 #include <iostream>
 using namespace std;
 
+//Struct utama
+struct Barang {
+  string id;
+  string nama;
+  int stok;
+  string rak;
+};
+
+const int MAX = 500;
+Barang barang[MAX];
+int n = 0;
+
+//Array
+//bikinka fungsi ini supaya bisa di testing
+void initData() {
+  //data dummy
+  n = 0;
+  barang[n++] = {"A1", "Tumbler", 20, "A"};
+  barang[n++] = {"A2", "Hair Dryer", 10, "A"};
+  barang[n++] = {"B1", "Ganci", 30, "B"};
+  barang[n++] = {"B2", "Tasbih Digital", 15, "B"};
+  barang[n++] = {"C1", "CCTV", 5, "C"};
+  barang[n++] = {"C2", "Senter", 25, "C"};
+  barang[n++] = {"D1", "Battery Realme", 12, "D"};
+  barang[n++] = {"D2", "Battery OPPO", 10, "D"};
+  barang[n++] = {"E1", "Battery Samsung", 8, "E"};
+  barang[n++] = {"E2", "Battery iPhone", 6, "E"};
+  barang[n++] = {"E3", "Kipas Portable", 20, "E"};
+  barang[n++] = {"F1", "Stylus", 18, "F"};
+  barang[n++] = {"F2", "Kran Air", 7, "F"};
+  barang[n++] = {"G1", "Ampere Meter", 4, "G"};
+  barang[n++] = {"G2", "Router", 9, "G"};
+  barang[n++] = {"H1", "LCD", 3, "H"};
+  barang[n++] = {"H2", "Cup Press", 2, "H"};
+  barang[n++] = {"I1", "Mouse", 25, "I"};
+  barang[n++] = {"I2", "Keyboard", 15, "I"};
+  barang[n++] = {"J1", "Kabel Charger", 40, "J"};
+  barang[n++] = {"J2", "Kepala Charger", 35, "J"};
+  barang[n++] = {"K1", "Case Xiaomi", 22, "K"};
+  barang[n++] = {"K2", "Case iPhone", 18, "K"};
+  barang[n++] = {"K3", "Case Samsung", 20, "K"};
+}
+//NOTE : tolong di cek coba, struct ini sudah sesuai blum?
 
