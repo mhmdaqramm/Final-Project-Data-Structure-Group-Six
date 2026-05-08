964eead3c14e3b3297f51f87e3398292425c1e41
cca489 <nursyalsabila6@gmail.com>
2026-05-08 21:36:48 +0800

    Update linksearch.cpp

--- linksearch.cpp
+++ linksearch.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#inlcude <string>
+#include <string>
+#include <cctype>
 using namespace std;
 
 struct NodeLL {
@@ -29,4 +30,33 @@ void tampilRiwayat() {
         cout<<"— "<<t->aktivitas<<endl;
         t=t->next;
     }
+}
+
+string kapital(string s) {
+    for(int i=0;i<s.length();i++) {
+        s[i]=toupper(s[i]);
+    }
+    return s;
+}
+
+int binarySearch(string x) {
+    int l=0,r=n-1;
+    while(l<=r) {
+        int mid=(l+r)/2;
+        if(barang[mid].id==x) return mid;
+        if(barang[mid].id<x) l=mid+1;
+        else r=mid-1;
+    }
+    return -1;
+}
+
+void linearSearch(string nama) {
+    bool ada=false;
+    for(int i=0;i<n;i++) {
+        if(kapital(barang[i].nama)==kapital(nama)) {
+            cout<<"Ditemukan ID "<<barang[i].id<<" di rak "<<barang[i].rak<<endl;
+            ada=true;
+        }
+    }
+    if(!ada) cout<<"Tidak ditemukan\n";
 }
